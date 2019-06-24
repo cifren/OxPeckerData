cb133870722f3910a5c8851707ca6e2eefdff706
Cifren <cifren@msn.com>
2019-06-24 22:37:59 +0200

    dev: Add test dataProcess with etlProcess

--- Tests/Core/DataProcessTest.php
+++ Tests/Core/DataProcessTest.php
@@ -6,52 +6,81 @@ use Cifren\OxPeckerData\Core\DataProcess;
 use Cifren\OxPeckerData\Definition\DataConfigurationInterface;
 use Cifren\OxPeckerData\Model\Stopwatch;
 use PHPUnit\Framework\TestCase;
+use Cifren\OxPeckerData\ETL\Core\IterationETLProcess;
 
 class DataProcessTest extends TestCase
 {
     public function testProcess(): void
     {
-        list($dataProcess, $configuration) = $this->getProcessMocks();
+        list($dataProcess,
+            $configurationWithoutETLProcesses,
+            $configurationWithETLProcesses
+        ) = $this->getProcessMocks();
 
-        $dataProcess->process($configuration, []);
+        $dataProcess->process($configurationWithoutETLProcesses);
+
+        $dataProcess->process($configurationWithETLProcesses);
     }
 
     public function getProcessMocks()
     {
-        $configuration = $this->createMock(DataConfigurationInterface::class);
-        $configuration
+        $iterationProcess = $this->createMock(IterationETLProcess::class);
+        $iterationProcess
             ->expects($this->atLeastOnce())
-            ->method("preProcess");
-        $configuration
+            ->method('setContext');
+        $iterationProcess
             ->expects($this->atLeastOnce())
-            ->method("getETLProcesses")
+            ->method('getContext')
+            ->willReturn(false);
+        $iterationProcess
+            ->expects($this->atLeastOnce())
+            ->method('process');
+
+        $configurationWithoutETLProcesses = $this->createMock(DataConfigurationInterface::class);
+        $configurationWithoutETLProcesses
+            ->expects($this->atLeastOnce())
+            ->method('preProcess');
+        $configurationWithoutETLProcesses
+            ->expects($this->atLeastOnce())
+            ->method('getETLProcesses')
             ->willReturn([]);
-        $configuration
+        $configurationWithoutETLProcesses->expects($this->atLeastOnce())
+            ->method('postProcess');
+
+        $configurationWithETLProcesses = $this->createMock(DataConfigurationInterface::class);
+        $configurationWithETLProcesses
             ->expects($this->atLeastOnce())
-            ->method("postProcess");
-        
+            ->method('preProcess');
+        $configurationWithETLProcesses
+            ->expects($this->atLeastOnce())
+            ->method('getETLProcesses')
+            ->willReturn([$iterationProcess]);
+        $configurationWithETLProcesses->expects($this->atLeastOnce())
+            ->method('postProcess');
+
         $stopWatch = $this->createMock(Stopwatch::class);
         $stopWatch
             ->expects($this->atLeastOnce())
-            ->method("start");
+            ->method('start');
         $stopWatch
             ->expects($this->atLeastOnce())
-            ->method("stop");
-        
-        $dateinterval = $this->createMock(\DateInterval::class);        
+            ->method('stop');
+
+        $dateinterval = $this->createMock(\DateInterval::class);
         $dateinterval
             ->expects($this->atLeastOnce())
-            ->method("format");
+            ->method('format');
         $stopWatch
             ->expects($this->atLeastOnce())
-            ->method("getFinishTime")
+            ->method('getFinishTime')
             ->willReturn($dateinterval);
-        
+
         $dataProcess = new DataProcess($stopWatch);
 
         return [
             $dataProcess,
-            $configuration,
+            $configurationWithoutETLProcesses,
+            $configurationWithETLProcesses,
         ];
     }
 }
